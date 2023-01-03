a90dcdaa8e4723e01754fd7d6a3d0984df511316
Kiriyako <92046315+Kiriyako@users.noreply.github.com>
2023-01-03 19:46:35 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,46 @@
+fetch('https://gogoanime.consumet.org/recent-release')
+  .then(response => response.json())
+  .then(data => {
+    let html = "";
+    const recent = document.getElementById("recentrelease");
+    data.slice(0, 1).forEach(anime => {
+      html += `
+           <img height="350" width="250" src="${anime.animeImg}" style="display: flex;"> <p></p>
+         <a href="https://kiriyako.github.io/amai/watch?query=${anime.animeId}&ep=${anime.episodeNum}"> <div id="rr"> ${anime.animeTitle}  <br> 
+           Episode ${anime.episodeNum} </a> </div>
+      `;
+    });
+
+    recent.innerHTML = `<h1>Latest Release</h1>` + html;
+  });
+
+  fetch('https://gogoanime.consumet.org/popular')
+  .then(response => response.json())
+  .then(data => {
+    let html = "";
+    const recent = document.getElementById("popular");
+    data.slice(0, 1).forEach(anime => {
+      html += `
+           <img height="350" width="250" src="${anime.animeImg}" style="display: flex;"> <p></p>
+         <a href="https://kiriyako.github.io/amai/anime?query=${anime.animeId}"> <div id="rr"> ${anime.animeTitle}  <br> 
+           </div>
+      `;
+    });
+
+    recent.innerHTML = `<h1>Popular Right Now</h1>` + html;
+  });
+
+  fetch('https://gogoanime.consumet.org/top-airing')
+  .then(response => response.json())
+  .then(data => {
+    let html = "";
+    const recent = document.getElementById("slideshow");
+    data.slice(0, 1).forEach(anime => {
+      html += `<img height="350" width="250" src="${anime.animeImg}" style="display: flex;"> <p></p>
+      <a href="https://kiriyako.github.io/amai/anime?query=${anime.animeId}"> <div id="rr"> ${anime.animeTitle}  <br> 
+        </div>
+      `;
+    });
+
+    recent.innerHTML = html;
+  });
--- js/search.js
+++ js/search.js
@@ -19,3 +19,21 @@ for (var i = 0; i < data.length; i++) {
   cardDiv.appendChild(animeDiv);
 }
 });
+
+fetch('https://gogoanime.consumet.org/search?keyw='+ query)
+.then(response => response.json())
+.then(data => {
+    var cardDiv = document.getElementById("card");
+
+for (var i = 0; i < data.length; i++) {
+  var anime = data[i];
+  var animeDiv = document.createElement("div");
+  animeDiv.style.display = "inline-block";
+  animeDiv.style.marginBottom = "20px";
+  animeDiv.style.width = "300px";
+
+  animeDiv.innerHTML = `<img height="350" width="250" src="${anime.animeImg}" alt="${anime.animeTitle}"> <a href="https://kiriyako.github.io/amai/anime?query=${anime.animeId}"  </a> <h2>${anime.animeTitle}</h2>`;
+  cardDiv.appendChild(animeDiv);
+}
+});
+
