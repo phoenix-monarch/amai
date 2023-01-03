1c0bcc98c93df72b637742d44d61b3bc304de6f4
Kiriyako <92046315+Kiriyako@users.noreply.github.com>
2023-01-03 19:49:17 +0530

    Add files via upload

--- js/search.js
+++ js/search.js
@@ -20,20 +20,3 @@ for (var i = 0; i < data.length; i++) {
 }
 });
 
-fetch('https://gogoanime.consumet.org/search?keyw='+ query)
-.then(response => response.json())
-.then(data => {
-    var cardDiv = document.getElementById("card");
-
-for (var i = 0; i < data.length; i++) {
-  var anime = data[i];
-  var animeDiv = document.createElement("div");
-  animeDiv.style.display = "inline-block";
-  animeDiv.style.marginBottom = "20px";
-  animeDiv.style.width = "300px";
-
-  animeDiv.innerHTML = `<img height="350" width="250" src="${anime.animeImg}" alt="${anime.animeTitle}"> <a href="https://kiriyako.github.io/amai/anime?query=${anime.animeId}"  </a> <h2>${anime.animeTitle}</h2>`;
-  cardDiv.appendChild(animeDiv);
-}
-});
-
