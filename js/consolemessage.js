423a164cdac24dc23f381fae88406ea50fad5e12
Kiriyako <92046315+Kiriyako@users.noreply.github.com>
2023-01-12 19:59:40 +0530

    Add files via upload

--- js/consolemessage.js
+++ js/consolemessage.js
@@ -1,1 +1,1 @@
-console.log("%cDo NOT paste anything here unless you know what you are doing! If you do know what you're doing, you might want to help us with amai project. https://github.com/Kiriyako/amai  With <3 from team amai", "color:pink; font-family:monospace; font-size: 24px");
+console.log("%cDo NOT paste anything here unless you know what you are doing! If you do know what you're doing, you might want to help us with amai project. https://github.com/Kiriyako/amai  With <3 from team amai", "color:#0098FF; font-family:monospace; font-size: 24px");
