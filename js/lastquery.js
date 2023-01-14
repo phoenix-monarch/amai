c251f0350d1404e1712ee2e98ad8fb3598e97960
Kiriyako <92046315+Kiriyako@users.noreply.github.com>
2023-01-14 13:56:21 +0530

    Delete lastquery.js

--- js/lastquery.js
+++
@@ -1,9 +0,0 @@
-
-//Code for searching the last query the user made
-const queryInput = document.getElementById("query");
-if (localStorage.getItem("query")) {
-  queryInput.value = localStorage.getItem("query");
-}
-queryInput.addEventListener("input", function() {
-  localStorage.setItem("query", this.value);
-});
